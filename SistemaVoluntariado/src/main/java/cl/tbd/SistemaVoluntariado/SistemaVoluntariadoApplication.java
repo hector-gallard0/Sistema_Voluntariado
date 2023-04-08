720dff9a091bbff7f08b2afa926754a7b5248ee6
hector-gallard0 <hector.gallardo.a@usach.cl>
2023-04-08 15:14:21 -0400

    first commit backend

---
+++ SistemaVoluntariado/src/main/java/cl/tbd/SistemaVoluntariado/SistemaVoluntariadoApplication.java
@@ -0,0 +1,13 @@
+package cl.tbd.SistemaVoluntariado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVoluntariadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVoluntariadoApplication.class, args);
+	}
+
+}
---
+++ SistemaVoluntariado/src/test/java/cl/tbd/SistemaVoluntariado/SistemaVoluntariadoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tbd.SistemaVoluntariado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVoluntariadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
