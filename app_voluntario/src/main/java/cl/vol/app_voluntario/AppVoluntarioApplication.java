7a241f125a70919c4dd3c0a3716b2ec6a481d213
hector-gallard0 <hector.gallardo.a@usach.cl>
2023-04-08 17:30:51 -0400

    proyecto spring boot con gradle funcionando

---
+++ app_voluntario/src/main/java/cl/vol/app_voluntario/AppVoluntarioApplication.java
@@ -0,0 +1,13 @@
+package cl.vol.app_voluntario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVoluntarioApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AppVoluntarioApplication.class, args);
+	}
+
+}
---
+++ app_voluntario/src/test/java/cl/vol/app_voluntario/AppVoluntarioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.vol.app_voluntario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVoluntarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
