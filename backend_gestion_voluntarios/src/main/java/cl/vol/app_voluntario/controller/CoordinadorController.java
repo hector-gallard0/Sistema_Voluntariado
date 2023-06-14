fce194be771004bd2319b3c2fc3fb2fe45c34dc9
Nicolás Rojas <64169554+onicalls@users.noreply.github.com>
2023-06-14 16:53:13 -0400

    CoordinadorController and EmergenciaController update

--- backend_gestion_voluntarios/src/main/java/cl/vol/app_voluntario/controller/CoordinadorController.java
+++ backend_gestion_voluntarios/src/main/java/cl/vol/app_voluntario/controller/CoordinadorController.java
@@ -1,13 +1,10 @@
 package cl.vol.app_voluntario.controller;
 
-import cl.vol.app_voluntario.response.ApiResponse;
 import cl.vol.app_voluntario.service.CoordinadorService;
-import cl.vol.app_voluntario.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- backend_gestion_voluntarios/src/main/java/cl/vol/app_voluntario/controller/EmergenciaController.java
+++ backend_gestion_voluntarios/src/main/java/cl/vol/app_voluntario/controller/EmergenciaController.java
@@ -1,27 +1,18 @@
 package cl.vol.app_voluntario.controller;
 
-import cl.vol.app_voluntario.model.Emergencia;
-import cl.vol.app_voluntario.model.Usuario;
-import cl.vol.app_voluntario.repository.EmergenciaRepository;
-import cl.vol.app_voluntario.request.CreateEmergenciaRequest;
-import cl.vol.app_voluntario.request.CreateTareaRequest;
-import cl.vol.app_voluntario.request.RegisterRequest;
-import cl.vol.app_voluntario.request.UpdateEmergenciaRequest;
+import cl.vol.app_voluntario.request.*;
 import cl.vol.app_voluntario.response.ApiResponse;
-import cl.vol.app_voluntario.response.AuthenticationResponse;
 import cl.vol.app_voluntario.service.EmergenciaService;
 import cl.vol.app_voluntario.util.ValidationUtil;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -43,12 +34,15 @@ public class EmergenciaController {
     @GetMapping("/emergencias")
     public ResponseEntity<?> getEmergencias(){
         return new ResponseEntity<>(emergenciaService.getEmergencias(), HttpStatus.OK);
-    };
+    }
 
-    //TAREA UPDATE
+    //UPDATE
     @PutMapping("/emergencias/{id}")
-    public ResponseEntity<?> updateEmergencia(@PathVariable Integer id,
-                                        @Valid @RequestBody UpdateEmergenciaRequest request){
+    public ResponseEntity<?> updateEmergencia(
+            @Valid @NotNull @PathVariable Integer id,
+            @Valid @RequestBody UpdateEmergenciaRequest request,
+            BindingResult bindingResult){
+        if(bindingResult.hasErrors()) return new ResponseEntity<>(ValidationUtil.getValidationErrors(bindingResult), HttpStatus.BAD_REQUEST);
         emergenciaService.updateEmergencia(id, request);
         Map<String, String> messages = new HashMap<>();
         messages.put("exito", "Emergencia editada con éxito.");
@@ -59,11 +53,68 @@ public class EmergenciaController {
                         .messages(messages)
                         .build(),
                         HttpStatus.OK);
-    };
+    }
 
-    //TAREA UPDATE
+    @DeleteMapping("/emergencias/{idEmergencia}")
+    public ResponseEntity<?> deleteEmergencia(@Valid @NotNull  @PathVariable Integer idEmergencia,
+                                              BindingResult bindingResult){
+        if(bindingResult.hasErrors()) return new ResponseEntity<>(ValidationUtil.getValidationErrors(bindingResult), HttpStatus.BAD_REQUEST);
+        emergenciaService.deleteEmergencia(idEmergencia);
+        Map<String, String> messages = new HashMap<>();
+        messages.put("exito", "Emergencia eliminada con éxito.");
+
+        return new ResponseEntity<>
+                (new ApiResponse().builder()
+                        .status(HttpStatus.OK.value())
+                        .messages(messages)
+                        .build(),
+                        HttpStatus.OK);
+    }
+
+
+    //OBTENER VOLUNTARIOS DE UNA EMERGENCIA
     @GetMapping("/emergencias/{id}/voluntarios")
     public ResponseEntity<?> getVoluntariosEmergencia(@PathVariable Integer id) {
         return new ResponseEntity<>(emergenciaService.getVoluntariosEmergencia(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/emergencias/{idEmergencia}/habilidades")
+    public ResponseEntity<?> getHabilidadesEmergencia(@Valid @NotNull @PathVariable Integer idEmergencia, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) return new ResponseEntity<>(ValidationUtil.getValidationErrors(bindingResult), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(emergenciaService.getHabilidadesEmergencia(idEmergencia), HttpStatus.OK);
+    }
+
+    @PostMapping("/emergencias/{idEmergencia}/habilidades/{idHabilidad}")
+    public ResponseEntity<?> addHabilidadEmergencia(
+            @Valid @NotNull @PathVariable Integer idEmergencia,
+            @Valid @NotNull @PathVariable Integer idHabilidad,
+            BindingResult bindingResult
+    ){
+        if(bindingResult.hasErrors()) return new ResponseEntity<>(ValidationUtil.getValidationErrors(bindingResult), HttpStatus.BAD_REQUEST);
+        emergenciaService.addHabilidadEmergencia(idEmergencia, idHabilidad);
+        return new ResponseEntity<>("Éxito", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/emergencias/{idEmergencia}/habilidades/{idHabilidad}")
+    public ResponseEntity<?> deleteHabilidadEmergencia(
+            @Valid @NotNull @PathVariable Integer idEmergencia,
+            @Valid @NotNull @PathVariable Integer idHabilidad,
+            BindingResult bindingResult
+    ){
+        if(bindingResult.hasErrors()) return new ResponseEntity<>(ValidationUtil.getValidationErrors(bindingResult), HttpStatus.BAD_REQUEST);
+        emergenciaService.deleteHabilidadEmergencia(idEmergencia, idHabilidad);
+        return new ResponseEntity<>("Éxito", HttpStatus.OK);
+    }
+
+    @PutMapping("/emergencias/{idEmergencia}/habilidades/{idHabilidad}")
+    public ResponseEntity<?> updateEmeHabilidad(
+            @Valid @NotNull @PathVariable Integer idEmergencia,
+            @Valid @NotNull @PathVariable Integer idHabilidad,
+            @Valid @RequestBody UpdateEmeHabilidadRequest request,
+            BindingResult bindingResult
+    ){
+        if(bindingResult.hasErrors()) return new ResponseEntity<>(ValidationUtil.getValidationErrors(bindingResult), HttpStatus.BAD_REQUEST);
+        emergenciaService.updateHabilidadEmergencia(idEmergencia, idHabilidad, request.getIdHabilidad());
+        return new ResponseEntity<>("Éxito", HttpStatus.OK);
     }
 }
