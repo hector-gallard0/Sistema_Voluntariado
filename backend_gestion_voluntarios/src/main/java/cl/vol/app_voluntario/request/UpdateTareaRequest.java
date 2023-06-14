11931626717af1b8aa90ac6c853b56b3c5f6f4e7
Clemente Aguilar <clemente.aguilar@usach.cl>
2023-06-14 16:41:46 -0400

    Request U Tarea Final

--- backend_gestion_voluntarios/src/main/java/cl/vol/app_voluntario/request/UpdateTareaRequest.java
+++ backend_gestion_voluntarios/src/main/java/cl/vol/app_voluntario/request/UpdateTareaRequest.java
@@ -1,4 +1,10 @@
 package cl.vol.app_voluntario.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class UpdateTareaRequest {
+    @NotNull
+    Integer idTarea;
 }
